ec85805b3ec29ef74c1920172d595494cea7e636
Raghav <raghavwandre1234@gmail.com>
2021-11-04 16:05:44 +0530

    Address Book

---
+++ AddressBook.sql
@@ -0,0 +1,26 @@
+create table AddressBook
+(fname varchar(20),lname varchar(20),address varchar(20),city varchar(20),state varchar(20),zip int,phone int);
+
+insert into AddressBook
+values
+('Raghav','Wandre','Anupam Nagar','Mumbai','Maharashtra',400001,9871964315)
+
+update AddressBook set phone=9975842285
+where fname='Raghav';
+
+delete AddressBook where fname='Raghav';
+
+select * from AddressBook where city='Mumbai' or state='Goa';
+
+select count(city) cityCount,count(state) stateCount from AddressBook;
+
+select * from AddressBook where city='Mumbai' order by fname;
+
+alter table AddressBook 
+add type varchar(20) default 'NA',name varchar(20) default 'NA';
+
+select count(type) from AddressBook;
+
+update AddressBook set type='Family' where fname='Raghav';
+
+select * from AddressBook;
